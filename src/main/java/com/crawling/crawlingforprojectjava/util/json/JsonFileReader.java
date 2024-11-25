9a5757ab5538e2d5a6721fd222e2d5b64eaeb12c
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-11-25 20:32:58 +0900

    feat: jsonFile 읽어서 원하는 타입으로 변경 후 리스트 반환

--- src/main/java/com/crawling/crawlingforprojectjava/util/json/JsonFileReader.java
+++ src/main/java/com/crawling/crawlingforprojectjava/util/json/JsonFileReader.java
@@ -1,15 +1,17 @@
 package com.crawling.crawlingforprojectjava.util.json;
 
-import com.crawling.crawlingforprojectjava.jobplanet.domain.JobPlanetCompanyInfo;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * packageName    : com.crawling.crawlingforprojectjava.util
@@ -22,22 +24,28 @@ import java.nio.file.Path;
  * -----------------------------------------------------------
  * 24. 11. 25.        sjunpark       최초 생성
  */
+@Slf4j
 public class JsonFileReader {
     private static final ObjectMapper objectMapper = new ObjectMapper();
     
-    public static void readFrom(@NonNull String jsonFilePath) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonFactory factory = objectMapper.getFactory();
-        JsonParser parser = factory.createParser(Files.newInputStream(Path.of(jsonFilePath)));
+    public static <T> List<T> readFrom(@NonNull String jsonFilePath, @NonNull Class<T> clazz) {
+        List<T> entities = new ArrayList<>();
         
-        // JSON 배열 시작 확인
-        if (parser.nextToken() == JsonToken.START_ARRAY) {
-            while (parser.nextToken() != JsonToken.END_ARRAY) {
-                // 각 객체를 읽어서 MyEntity로 변환
-                JobPlanetCompanyInfo companyInfo = objectMapper.readValue(parser, JobPlanetCompanyInfo.class);
+        JsonFactory factory = objectMapper.getFactory();
+        
+        try (JsonParser parser = factory.createParser(Files.newInputStream(Path.of(jsonFilePath)));) {
+            // JSON 배열 시작 확인
+            if (parser.nextToken() == JsonToken.START_ARRAY) {
+                while (parser.nextToken() != JsonToken.END_ARRAY) {
+                    T info = objectMapper.readValue(parser, clazz);
+                    entities.add(info);
+                }
             }
+            
+        } catch (IOException e) {
+            log.error("JsonFileReader.readFrom() : {}", e.getMessage());
         }
-        parser.close();
+        
+        return entities;
     }
-    
 }
