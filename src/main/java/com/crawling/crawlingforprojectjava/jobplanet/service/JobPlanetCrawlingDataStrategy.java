e3d7045f87760697ec104a286d4041a644e84577
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-11-26 23:00:28 +0900

    test: 데이터 저장 관련 크롤링 등

--- src/main/java/com/crawling/crawlingforprojectjava/jobplanet/service/JobPlanetCrawlingDataStrategy.java
+++ src/main/java/com/crawling/crawlingforprojectjava/jobplanet/service/JobPlanetCrawlingDataStrategy.java
@@ -1,8 +1,13 @@
-package com.crawling.crawlingforprojectjava.crawling_data;
+package com.crawling.crawlingforprojectjava.jobplanet.service;
 
+import com.crawling.crawlingforprojectjava.crawling_data.CrawlingStrategy;
+import com.crawling.crawlingforprojectjava.jobplanet.domain.JobPlanetCompanyInfo;
 import com.crawling.crawlingforprojectjava.jobplanet.repository.JobPlanetCompanyInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * packageName    : com.crawling.crawlingforprojectjava.crawling_data
@@ -16,12 +21,22 @@ import org.springframework.stereotype.Service;
  * 24. 11. 26.        sjunpark       최초 생성
  */
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class JobPlanetCrawlingDataStrategy implements CrawlingStrategy {
     private final JobPlanetCompanyInfoRepository jobPlanetCompanyInfoRepository;
     
+    @Transactional(readOnly = false)
     @Override
-    public void saveCrawlingData() {
+    public void saveCrawlingData(List<JobPlanetCompanyInfo> datas) {
+        final int batchSize = 500;
+        final int dataSize = datas.size();
         
+        for (int i = 0; i < dataSize; i += batchSize) {
+            int endIdx = Math.min(i + batchSize, dataSize);
+            
+            jobPlanetCompanyInfoRepository.saveAll(datas.subList(i, endIdx));
+            jobPlanetCompanyInfoRepository.flush();
+        }
     }
 }
