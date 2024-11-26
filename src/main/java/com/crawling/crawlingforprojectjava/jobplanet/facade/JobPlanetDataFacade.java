8489ed8f49b791b29be60898948ecb80c69efbe9
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-11-26 23:19:42 +0900

    feat: 데이터 저장 작업 스케쥴링 및 FACADE 계층정의

---
+++ src/main/java/com/crawling/crawlingforprojectjava/jobplanet/facade/JobPlanetDataFacade.java
@@ -0,0 +1,34 @@
+package com.crawling.crawlingforprojectjava.jobplanet.facade;
+
+import com.crawling.crawlingforprojectjava.common.FileDataStore;
+import com.crawling.crawlingforprojectjava.crawling_data.CrawlingStrategy;
+import com.crawling.crawlingforprojectjava.jobplanet.domain.JobPlanetCompanyInfo;
+import com.crawling.crawlingforprojectjava.util.json.JsonFileReader;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * packageName    : com.crawling.crawlingforprojectjava.jobplanet.facade
+ * fileName       : JobPlanetDataFacade
+ * author         : ipeac
+ * date           : 24. 11. 26.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 11. 26.        ipeac       최초 생성
+ */
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JobPlanetDataFacade {
+    private final CrawlingStrategy jobPlanetCrawlingDataStrategy;
+    
+    public void processCrawlingData() {
+        final List<JobPlanetCompanyInfo> jobPlanetCompanyInfos = JsonFileReader.readFrom(FileDataStore.JOB_PLANET_CRAWLING_FILE_PATH.getFilePath(), FileDataStore.JOB_PLANET_CRAWLING_FILE_PATH.getClazz());
+        jobPlanetCrawlingDataStrategy.saveCrawlingData(jobPlanetCompanyInfos);
+    }
+}
